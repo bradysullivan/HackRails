936445c4601727c97183c0c26a1c583d382f2a23
bradysullivan <brady.sullivan@iwsinc.com>
2013-03-01 11:21:12 -0800

    Initial rails build.
    Summary: Ruby (1.9.3) Rails (3.2.12)
    
    Test Plan: None.
    
    Reviewers: debus.phil
    
    Reviewed By: debus.phil
    
    Differential Revision: http://hackphab.tk/D3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackRails::Application.config.secret_token = '166ad4addece936d2de8a78b58a468d82dc4dfd4aaa1a25e0b4f4a97dca12c834409d6a878306a3c463a59e560a82015857739771e6bd4f29308c6c6e64e72a6'
