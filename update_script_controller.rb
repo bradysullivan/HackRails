dd839cd0f17375a9a7b1be515c1dd16157bb6470
phildebus <pdebus@iwsinc.com>
2013-03-04 16:51:28 -0800

    Add backtracing to update_script_controller.rb

--- update_script_controller.rb
+++ update_script_controller.rb
@@ -2,9 +2,9 @@ require 'rubygems'        # if you use RubyGems
 require 'daemons'
 
 options = {
-  :app_name   => "HackRails_Updater",
+  :app_name   => "hack_rails_updater",
   :backtrace  => true,
 }
 
 
-Daemons.run('update_script.rb')
+Daemons.run('update_script.rb', options)
