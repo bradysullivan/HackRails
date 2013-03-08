0094335c5e1b9292b83ae84a9cb0208cb0eb1945
phildebus <pdebus@iwsinc.com>
2013-03-07 16:33:53 -0800

    Change commits to be serialized as a Hash for easier access

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,6 +1,6 @@
 class Update < ActiveRecord::Base
   attr_accessible :after, :before, :commits, :ref
-  serialize :commits, Array
+  serialize :commits, Hash
 
   def self.allowed_ips
     %w[207.97.227.253 50.57.128.197 108.171.174.178 50.57.231.61 204.232.175.64 192.30.252.0 204.232.175.75]
